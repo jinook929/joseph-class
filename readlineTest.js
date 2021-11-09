2d55befc7a4469f31b325dbd0933c0efec488ec7
jinook929 <jinook929@gmail.com>
2021-11-08 22:42:30 -0500

    finished clock hands degree calculator

--- readlineTest.js
+++ readlineTest.js
@@ -1,4 +1,16 @@
 const prompt = require('prompt-sync')({sigint: true});
 
-const num = prompt('Enter a number: ');
-console.log(`Your number + 4 = ${Number(num) + 4}`);
+// const num = prompt('Enter a number: ');
+// console.log(`Your number + 4 = ${Number(num) + 4}`);
+
+const hour = prompt('Enter an hour value (1-12): ');
+const minute = prompt('Enter a minute value (0-59): ');
+
+const minuteHand = 360 * (minute / 60);
+const hourHand = (360 * (hour / 12) % 360) + (30 * (minute / 60));
+
+const angle = Math.abs(minuteHand - hourHand);
+
+console.log(minuteHand)
+console.log(hourHand)
+console.log(angle)
